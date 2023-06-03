c483e3ee030aabab94be41f61bf9f58b0441005a
Cxlopez <cristian4402@hotmail.com>
2023-06-03 16:17:00 -0400

    create body copmonent & styling

---
+++ src/components/Body.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import "../styles/Body.css";
+
+
+function Body() {
+  return (
+    <div>
+      <div className="container">
+        <img className='work-sign' src="https://github.com/Cxlopez/hi-perennia/assets/93356900/45f8ea40-623c-4f18-8a15-8bf20a0dbe0f" alt="fly-speech" />
+        <div className="box-container">
+          <div className="box">
+            <img src="https://github.com/Cxlopez/hi-perennia/assets/93356900/fbed6ffa-e3db-4e26-8d14-27e142551f9a" alt="c-img" />
+            <div className="box-content">
+              <h2 className="box-title">My Goal</h2>
+              <p>
+                My goal is to secure a position as a junior software developer at Perennia, a company known for its excellence. Joining Perennia would provide me with the opportunity to enhance my current skills and expand my knowledge as a developer. I'm mostly excited about the idea of collaborating with an elite group of developers, as this would help push me to new heights in my career. With determination and a passion for coding, I'm ready to contribute and learn from the best at Perennia.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="box-container">
+          <div className="box">
+            <img src="https://github.com/Cxlopez/hi-perennia/assets/93356900/afbb3f31-1161-479c-8d7e-9e9580079284" alt="c-img" />
+            <div className="box-content">
+              <h2 className="box-title">My Journey</h2>
+              <p>
+                I've always been fascinated by tech and art, from oil painting to building gaming computers I eventually found my passion for coding when playing with C# and unity as a hobby. This led me to pursue web development as a career path. I decided to attend Lighthouse Labs, where I withnessed the gratifying experience of bringing my creations to life (using ReactJs). Since then I have been refining my skills with the current languages I know as I'm not trying to be a jack of all trades and master of none.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="box-container">
+          <div className="box">
+            <img src="https://github.com/Cxlopez/hi-perennia/assets/93356900/b30cb211-4299-4bd3-ab34-40b5923b2f4c" alt="c-img" />
+            <div className="box-content">
+              <h2 className="box-title">About Me</h2>
+              <p>
+                In short I am a coder by day and gamer/ weight lifter by night! I also enjoy practicing MMA and reading books in my spare time. I'm a very social person, I love meeting new people and having big meals as a group (I love food), besides food always taste best when eating with friends! I'm known for having high energy levels but I'm also known for being "like a dad" since I always sleep so early... but who doesn't love sleep?? 😴
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Body
